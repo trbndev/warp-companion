f6dde3fbc2f195981941eb89b8f5eb8d85a82973
Torben Haack <haack.t@icloud.com>
2022-09-24 08:36:09 +0200

    fix: :bug: Fix #1

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,9 @@ import { initialActiviation, getColors, welcomeMessage } from './utils';
 
 export async function activate(context: vscode.ExtensionContext) {
 	vscode.window.onDidChangeActiveColorTheme(async ({ kind }) => {
+		if (!Warp.warpDirExists()) {
+			Warp.createWarpDir();
+		}
 		await getColors(Warp.writeWarpConfig, kind);
 	});
 
